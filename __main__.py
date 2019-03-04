c5cbedf57885d9bcb0c0b27c431c38da0cfa2db5
canvasnote <northfixiative@gmail.com>
2019-03-04 23:17:35 +0900

    add get_cards_list method

--- __main__.py
+++ __main__.py
@@ -1,4 +1,6 @@
+import argparse
 
+import get_cards_list
 
 if __name__ == "__main__":
     """
@@ -7,3 +9,12 @@ if __name__ == "__main__":
      -g --get-cards カードリストCSVからカードの記事を取得し、CSVに保存します。
      -h --help このヘルプを表示します。
     """
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-l", "--list", action="store_true")
+    parser.add_argument("-g", "--get-cards", action="store_true")
+    # parser.add_argument("-h", "--help", help="このヘルプを表示します。")
+    args = parser.parse_args()
+
+    if args.list:
+        get_cards_list.GetCardsList()
